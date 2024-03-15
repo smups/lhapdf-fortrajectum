daeaf653e5808196031186c1d5472c39552c94e4
Raul Wolters <56522208+smups@users.noreply.github.com>
2024-03-15 16:33:34 +0100

    pdf dir now automatically includes necessary config files

--- build.zig
+++ build.zig
@@ -21,43 +21,19 @@ const pdfs = [_][]const u8 {
 
     // Add option to set the data directory for LHAPDF
     const root_dir = b.build_root.handle;
-    const usr_ddir_path = b.option([]const u8, "data-dir", "");
+    const ddir_path = b.option([]const u8, "data-dir", "") orelse try init_default_ddir(alloc, root_dir);
     const install_pdfs = b.option(bool, "download-pdfs", "") orelse false;
 
-    // `ddir` is the root folder where all LHAPDF data is stored. `ddir/phf_dir` contains the actual
-    // pdfs. If the user does not 
-    const ddir = blk: {
-        if (usr_ddir_path) |path| {
-            break :blk std.fs.openDirAbsolute(path, .{}) catch |err| {
-                std.debug.print("Could not open data-dir {s} (is it an absolute path?) Err: \"{s}\"\n", .{
-                    path, @errorName(err)
-                });
-                return err;
-            };
-        } else {
-            break :blk root_dir.makeOpenPath(default_ddir, .{}) catch |err| {
-                 std.debug.print("Could not open default data-dir {s}. Err: \"{s}\"\n", .{
-                    try root_dir.realpathAlloc(alloc, default_ddir), @errorName(err)
-                });
-                return err;
-            };
-        }
-    };
-    const pdf_dir = ddir.makeOpenPath(pdf_dir_rel, .{}) catch |err| {
-        std.debug.print("Could not open pdf-dir {s}. Err: \"{s}\"", .{
-            try ddir.realpathAlloc(alloc, pdf_dir_rel), @errorName(err)
-        });
-        return err;
-    };
-    const ddir_path = try ddir.realpathAlloc(alloc, ".",);
+    const pdf_dir_path = try init_pdfdir(alloc, root_dir, ddir_path);
     defer alloc.free(ddir_path);
+    const pdf_dir = try std.fs.openDirAbsolute(pdf_dir_path, .{});
 
     if (install_pdfs) {
          try download_pdfs(alloc, pdf_dir);
     }
 
     // Load dependencies
-    const yaml_cpp = b.dependency("yaml_cpp_fortrajectum", .{
+    const yamlcpp = b.dependency("yamlcpp", .{
         .target = target,
         .optimize = optimize
     });
@@ -70,17 +46,16 @@ const pdfs = [_][]const u8 {
     });
 
     // Add dependency on yaml-cpp
-    lhapdf.linkLibrary(yaml_cpp.artifact("yaml-cpp-fortrajectum"));
+    lhapdf.linkLibrary(yamlcpp.artifact("yaml-cpp-fortrajectum"));
 
     // Add headers
     lhapdf.addIncludePath(.{ .path = "include/" });
-    lhapdf.addIncludePath(.{ .path = yaml_cpp.builder.h_dir });
 
     // Add source files
     const cpp_src = try list_cpp_src(alloc, try root_dir.openDir("src/", .{}));
     const cpp_flags = &.{
         "-std=c++11",
-        try std.fmt.allocPrint(alloc, "-DLHAPDF_DATA_PREFIX=\"{s}\"", .{ ddir_path })
+        b.fmt("-DLHAPDF_DATA_PREFIX=\"{s}\"", .{ ddir_path })
     };
     lhapdf.addCSourceFiles(cpp_src.items, cpp_flags);
 
@@ -91,6 +66,59 @@ const pdfs = [_][]const u8 {
     b.installArtifact(lhapdf);
 }
 
+fn init_default_ddir(alloc: Allocator, root: std.fs.Dir) ![]const u8 {
+    const ddir = root.makeOpenPath(default_ddir, .{}) catch |err| {
+        std.debug.print("[❌] Could create default data directory {s}. Err: \"{s}\"\n", .{
+            default_ddir, @errorName(err)
+        });
+        return err;
+    };
+    const ddir_path = try ddir.realpathAlloc(alloc, ".");
+    std.debug.print("[ℹ️] Using default data directory {s}\n", .{ ddir_path });
+    return ddir_path;
+}
+
+fn init_pdfdir(alloc: Allocator, root: std.fs.Dir, ddir_path: []const u8) ![]const u8 {
+    const ddir = std.fs.openDirAbsolute(ddir_path, .{}) catch |err| {
+        std.debug.print("[❌] Could not open data-dir {s} (is it an absolute path?) Err: \"{s}\"\n", .{
+            ddir_path, @errorName(err)
+        });
+        return err;
+    };
+    const pdf_dir = ddir.makeOpenPath(pdf_dir_rel, .{}) catch |err| {
+        std.debug.print("[❌] Could not open pdf-dir {s}. Err: \"{s}\"", .{
+            try ddir.realpathAlloc(alloc, pdf_dir_rel), @errorName(err)
+        });
+        return err;
+    };
+
+    // If pdf_dir/lhapdf.conf and pdf_dir/pdfsets.index do not exist, create them.
+    _ = pdf_dir.access("lhapdf.conf", .{}) catch |err| switch (err) {
+        error.FileNotFound => root.copyFile("lhapdf.conf", pdf_dir, "lhapdf.conf", .{}) catch |copyerr| {
+            // Copy file from package root to pdf dir
+            std.debug.print("[💣] THIS IS A BUG: packaging error", .{});
+            return copyerr;
+        },
+        else => {
+            std.debug.print("[❌] Cannot read lhapdf.conf file in pdf_dir. Err: \"{s}\"", .{ @errorName(err) });
+            return err;
+        } 
+    };
+    _ = pdf_dir.access("pdfsets.index", .{}) catch |err| switch (err) {
+        error.FileNotFound => root.copyFile("pdfsets.index", pdf_dir, "pdfsets.index", .{}) catch |copyerr| {
+            // Copy file from package root to pdf dir
+            std.debug.print("[💣] THIS IS A BUG: packaging error", .{});
+            return copyerr;
+        },
+        else => {
+            std.debug.print("[❌] Cannot read pdfsets.index file in pdf_dir. Err: \"{s}\"", .{ @errorName(err) });
+            return err;
+        } 
+    };
+
+    // Return path to pdf_dir
+    return try pdf_dir.realpathAlloc(alloc, ".");
+}
 
 /// This function traverses the `src_dir` and produces an `ArrayList` of all
 /// non-main source files in the `src_dir`.
@@ -102,7 +130,7 @@ fn list_cpp_src(alloc: Allocator, src_dir: std.fs.Dir) !std.ArrayList([]u8) {
             continue;
         }
         try source_files.append(try src_dir.realpathAlloc(alloc, entry.name));
-    }
+    }    
     return source_files;
 }
 
@@ -114,25 +142,30 @@ fn download_pdfs(alloc: Allocator, pdf_dir: std.fs.Dir) !void {
     defer headers.deinit();
     try headers.append("accept", "*/*");
     
+    const pdf_dir_path = try pdf_dir.realpathAlloc(alloc, ".");
+    defer alloc.free(pdf_dir_path);      
+    
     for (pdfs) |pdf_name| {        
         // Generate the uri to the .tar.gz file on the CERN server
         const url = try std.fmt.allocPrint(alloc, "{s}{s}.tar.gz", .{ pdf_svr_baseurl, pdf_name });
         defer alloc.free(url);
         const uri = try std.Uri.parse(url);        
-        std.debug.print("Downloading {s}. This could take a while...\n", .{url});
+        std.debug.print("[📥] Downloading {s}. This could take a while...\n", .{url});
 
         //Make the request
         var req = try client.request(std.http.Method.GET, uri, headers, .{});
         try req.start();
         try req.wait();
-        const buf = try req.reader().readAllAlloc(alloc, std.math.maxInt(usize));
-        std.debug.print("Finished downloading {s}. Received {} bytes.\n", .{pdf_name, buf.len});
+        const httpreader = req.reader();
 
-        //  Write file to disk
-        const fout_name = try std.fmt.allocPrint(alloc, "{s}.tar.gz", .{ pdf_name });
-        defer alloc.free(fout_name);
-        const out_file = try std.fs.Dir.createFile(pdf_dir, fout_name, .{});
-        defer out_file.close();
-        try out_file.writeAll(buf);
+        // Decompress the file
+        std.debug.print("[📂] Decompressing...\n", .{});
+        var decompressed_reader = try std.compress.gzip.decompress(alloc, httpreader);
+        defer decompressed_reader.deinit();
+
+        // Write to file system
+        try std.tar.pipeToFileSystem(pdf_dir, decompressed_reader.reader(), .{ .mode_mode = .ignore });
+      
+        std.debug.print("[✔️] Installed PDF \"{s}\" to {s}\n", .{ pdf_name, pdf_dir_path });
     }
 }
